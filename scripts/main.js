ab85ce6826a0b88ba7c0b3f31b393729fbf9c0bf
James Fisher <fisher.james@outlook.com>
2015-12-07 20:29:34 -0800

    Theoretically implement delayed content load and nav

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var pages = ["#bio", "#novantika", "#resume", "#listen", "#photos", "#links", "#studio"];
+
+$(document).ready(function() {
+  pages.forEach(function(page){
+    $(page).load("_posts/" + page + ".html");
+    $(page + "Nav").click(function(){switchContent(page)});
+  });
+  switchContent();
+	/*$("#bio").click(function(){switchContent("#bio")});
+  $("#novantika").click(function(){switchContent("#novantika")});
+  $("#resume").click(function(){switchContent("#resume")});
+  $("#listen").click(function(){switchContent("#listen")});
+  $("#photos").click(function(){switchContent("#photos")});
+  $("#links").click(function() {switchContent("#links")});
+  $("#studio").click(function() {switchContent("#studio")});*/
+  
+  
+});
+
+function switchContent(contentId) {
+  pages.forEach(function(page){
+    $(page).hide();
+  })
+
+  if (arguments.length > 0) {
+    $(contentId).show();
+  };
+};
